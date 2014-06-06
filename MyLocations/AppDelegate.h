de033b37710314b742eab937f79270c12f24e82d
jiangguimin <jiangguimin@funguide.com.cn>
2014-06-06 11:44:49 +0800

    Initial Commit

---
+++ MyLocations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by jiangguimin on 14-6-6.
+//  Copyright (c) 2014年 Dalnpo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyLocations/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyLocations
+//
+//  Created by jiangguimin on 14-6-6.
+//  Copyright (c) 2014年 Dalnpo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
